9518aa6e1849077d4aacd403e1af49469fadba83
unknown <md.asif.8393@gmail.com>
2023-12-13 12:47:21 +0600

    [Done --> Add Head tag and title for seo friendly]

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import RootLayout from "@/components/layouts/RootLayout";
+import Head from "next/head";
 
 
 const Homepage = () => {
 
   return (
     <div>
+      <Head>
+        <title>Next Home Page </title>
+        <meta name="home page" description="this page is created by next js" />
+      </Head>
       <h1>This is a Next.JS Home Page</h1>
     </div>
   )
